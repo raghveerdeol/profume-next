652e9f1bcb22e15cee6056f2f210c6f3a855590c
raghveerdeol <deolraghveer5@gmail.com>
2024-11-05 16:51:02 +0100

    added header css

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,8 +1,10 @@
+import '../style/header.css';
+
 export default function Navbar() {
     return (
         <>
             <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
+                <div className="container-fluid main-container">
                     <a className="navbar-brand">Navbar</a>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
